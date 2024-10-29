58c6de99fb10769b3550d04fcec045bf09929907
ibra <albakov.ibraguim@gmail.com>
2024-10-29 17:25:50 +0100

    [PRT-11]💄new components typing text animation

--- lib/core/design_system/app_ui.dart
+++ lib/core/design_system/app_ui.dart
@@ -2,8 +2,9 @@ library app_ui;
 
 import 'package:flutter/material.dart';
 
+export 'src/components/typing_text_animation.dart';
+
 part 'src/app_theme.dart';
 part 'src/data/app_theme_data.dart';
 part 'src/data/src/ui_color_data.dart';
 part 'src/data/src/ui_duration_data.dart';
-
